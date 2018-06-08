94604c1f8b91f6a69b9355caa63e65e85ba126a0
david <david.journo.31@gmail.com>
2018-06-08 14:18:18 +0200

    linting

--- server.js
+++ server.js
@@ -5,8 +5,17 @@ let server = require('http').createServer(app)
 let http = require('http');
 let io = require('socket.io').listen(server);
 let moment = require("moment");
+let tz = require('moment-timezone')
 let hour = moment().format('h:mm:ss');
 console.log(hour);
+
+// TIMEZONE
+let jun = moment("2014-06-01T12:00:00Z"); // SUMMER
+let dec = moment("2014-12-01T12:00:00Z"); // WINTER
+
+// ZONE
+let here = jun.tz('Europe/France').format('h:mm:ssa z');
+console.log(here)
 
 // USER NUMBER & CONNECTIONS
 users = [];
@@ -28,7 +37,6 @@ io.sockets.on('connection', function (socket) {
 
     // DISCONNECTED
     socket.on('disconnect', function (data) {
-        // if (!socket.username) return;
         users.splice(users.indexOf(socket.username), 1);
         connections.splice(connections.indexOf(socket), 1)
         console.log('Disconnected : %s Simplonien connecté', connections.length);
